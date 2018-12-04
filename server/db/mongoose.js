1e8260d7a55e5f6338d8a9dc6bbd6cd94afbf646
nxde <nxde@ctd.com>
2018-12-04 10:28:13 +0700

    Add POST /todos route

---
+++ server/db/mongoose.js
@@ -0,0 +1,35 @@
+const mongoose=require("mongoose");
+
+mongoose.Promise=global.Promise;
+mongoose.connect("mongodb://mead:mead123456@ds023435.mlab.com:23435/todoapp-mead",{ useNewUrlParser: true });
+
+
+
+// var newTodo=new Todo({
+//     text:"                  This text has been trimmed  2          "
+// });
+
+
+// newTodo.save().then(doc=>{
+//     console.log("Saved todo ",doc);
+// },err=>{
+//     console.log("Unable to save todo ",err);
+// });
+
+// var newUser=new User({
+//     email:"ali@gmail.com"
+// });
+
+// newUser.save().then(doc=>{
+//     console.log("Saved user ",doc);
+// },err=>{
+//     console.log("Unable to save user ",err);
+// });
+
+
+
+
+
+module.exports={
+    mongoose
+};
