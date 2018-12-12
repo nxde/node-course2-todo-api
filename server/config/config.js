e2bc58da6bb24abd099a6ce0610a66d1b73ab109
nxde <nxde@ctd.com>
2018-12-12 15:32:26 +0700

    User Schema generateAuthToken

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,10 @@ var env=process.env.NODE_ENV || "development";
 process.env.MONGODB_URI="mongodb://mead:mead123456@ds023435.mlab.com:23435/todoapp-mead";
 if(env==="development"){
     process.env.PORT=3000;//
-    //process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp";
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp";
 }
 else if (env==="test"){
     process.env.PORT=3000;
-    //process.env.MONGODB_URI="mongodb://localhost:27017/TodoAppTest"
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoAppTest"
     //process.env.MONGODB_URI="mongodb://mead:mead123456@ds023435.mlab.com:23435/todoapp-mead-Test";
 }
--- server/server.js
+++ server/server.js
@@ -96,6 +96,22 @@ app.patch("/todos/:id",(req,res)=>{
         }).catch(err=>res.status(400).send());
 });
 
+app.post("/users",(req,res)=>{
+    var body=_.pick(req.body,["email","password"]);
+    var user=new User(body);
+    //console.log("Newly created user,before saved: ",user);
+
+    user.save().then(()=>{
+        //res.send(user);
+        return user.generateAuthToken();
+    }).then(token=>{
+        res.header("x-auth",token).send(user);
+    }).catch(err=>{
+        res.status(400).send(err);
+    });
+});
+
+
 app.listen(PORT,()=>{
     console.log(`Server is up on port ${PORT}`);
 });
